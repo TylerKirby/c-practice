849729ee87f0d7245b0398da350eb71f18b4dccb
Tyler Kirby <tyler.kirby9398@gmail.com>
2020-04-23 20:59:17 -0500

    completed ex 17

--- the_c_book/chapter_1/ex_1_17.c
+++ the_c_book/chapter_1/ex_1_17.c
@@ -1,50 +1,29 @@
+/* Write a program to print all input lines that are longer than 
+ * 80 characters */
 #include <stdio.h>
+
 #define MAX_LINE 1000
 
-int get_line(char line[], int max_line);
-void copy(char to[], char from[]);
-
 int main() {
-    int len;
-    int max;
-    char line[MAX_LINE];
-    char longest[MAX_LINE];
+    int curr_char, line_incr;
+    char curr_line[MAX_LINE];
 
-    max = 0;
-    while ((len = get_line(line, MAX_LINE)) > 0) {
-        if (len > max) {
-            max = len;
-            copy(longest, line);
+    line_incr = 0;
+
+    while ((curr_char = getchar()) != EOF) {
+        if (curr_char != '\n') {
+            curr_line[line_incr] = curr_char;
+            ++line_incr;
+        } else {
+            if (line_incr > 80) {
+                // line is longer than 80 chars
+                curr_line[line_incr] = '\0';
+                printf("%s", curr_line);
+                line_incr = 0;
+            } else {
+                // line is not longer than 80 chars
+                line_incr = 0;
+            }
         }
-    }
-    if (max > 0) {
-        printf("Printing longest line of length %d\n", max);
-        printf("%s", longest);
-    }
-    return 0;
-}
-
-// get_line: read a line into s, return length
-int get_line(char s[], int lim) {
-    int c, i;
-
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-        s[i] = c;
-    }
-    if (c == '\n') {
-        s[i] = c;
-        ++i;
-    }
-    s[i] = '\0';
-    return i;
-}
-
-// copy: copy 'from' into 'to'; assume to is big enough
-void copy(char to[], char from[]) {
-    int i;
-
-    i = 0;
-    while ((to[i] = from[i]) != '\0') {
-        ++i;
     }
 }
