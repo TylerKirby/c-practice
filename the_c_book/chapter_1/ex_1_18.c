4445db12d8fce1e416d3e313c1eee391eb54e09c
Tyler Kirby <tyler.kirby9398@gmail.com>
2020-04-23 21:29:53 -0500

    started on ex 18

--- the_c_book/chapter_1/ex_1_18.c
+++ the_c_book/chapter_1/ex_1_18.c
@@ -6,18 +6,23 @@
 #define MAX_LINE_LENGTH 1000
 #define MAX_NUMBER_OF_LINES 1000
 
-int main {
-    int curr_char, curr_line;
+int main() {
+    int curr_char, line_incr, lines_incr, is_blank;
     char lines[MAX_LINE_LENGTH][MAX_NUMBER_OF_LINES];
 
-    curr_line = 0;
-    curr_
+    line_incr = lines_incr = is_blank = 0;
+
     while ((curr_char = getchar()) != EOF) {
-        if (curr_char == '\n') {
-            ++curr_line;
-
+        if (curr_char != '\n') {
+            lines[lines_incr][line_incr] = curr_char;
+            ++line_incr;
         } else {
+            lines[lines_incr][line_incr] = '\0';
+            ++lines_incr;
+        }
+    }
 
-        };
+    for (int i = 0; i < lines_incr; ++i) {
+        printf("%s\n", lines[i]);
     }
 };
