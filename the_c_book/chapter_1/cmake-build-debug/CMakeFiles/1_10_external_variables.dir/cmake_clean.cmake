07bc4276190bf271e111d1485bd967f13d3dc8b5
Tyler Kirby <tyler.kirby9398@gmail.com>
2020-05-01 08:54:08 -0500

    start ex 1 20

---
+++ the_c_book/chapter_1/cmake-build-debug/CMakeFiles/1_10_external_variables.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1_10_external_variables"
+  "1_10_external_variables.pdb"
+  "CMakeFiles/1_10_external_variables.dir/1_10_external_variables.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1_10_external_variables.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
